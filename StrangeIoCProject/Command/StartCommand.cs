3e7773cc7498eea844be29f037b7928cd6b81ada
KirbyXcode <kirby_yu@hotmail.com>
2017-10-01 16:20:41 +0800

    Add files via upload

---
+++ StrangeIoCProject/Command/StartCommand.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.command.impl;
+
+//开始命令
+public class StartCommand : Command {
+
+    /// <summary>
+    /// 当这个命令被执行的时候，默认会调用Excute方法
+    /// </summary>
+    public override void Execute()
+    {
+        Debug.Log("start command execute");
+    }
+}
